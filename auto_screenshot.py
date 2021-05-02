1ab1e8d4bd232aa666285d3f17b26e16929e1434
epter <82642114+epter@users.noreply.github.com>
2021-05-02 18:40:12 +0900

    Update auto_screenshot.py
    add keyboard set

--- auto_screenshot.py
+++ auto_screenshot.py
@@ -7,3 +7,18 @@ for i in range(1,11): #2초간격으로 10개의 이미지를 가져옴
     img = ImageGrab.grab() #현재 스크린 이미지 가져옴
     img.save("image{}.png".format(i)) # 파일로 저장 (image.png ~ image10.png)
     time.sleep(2) #2초 단위
+
+    
+####keyboard    
+# import keyboard
+# import time
+# from PIL import ImageGrab
+
+# def screenshot():
+#     curr_time = time.strftime("_%Y%m%d_%H%M%S")
+#     img = ImageGrab.grab()
+#     img.save("image{}.png".format(curr_time))
+
+# keyboard.add_hotkey("F9",screenshot)
+
+# keyboard.wait("esc")
